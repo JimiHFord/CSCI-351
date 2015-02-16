ca73d5dbd000df629ed95ffa0ddb45d251df5441
Jimi Ford <jford@vistaprint.com>
2015-02-16 11:54:06 -0500

    parallel implemented!
    still have to check if the results are correct - or depth is measured
    correctly

--- Project-1/Simulation.java
+++ Project-1/Simulation.java
@@ -8,46 +8,37 @@ public class Simulation {
 
 	private int v, n;
 	private double p;
-	private Random prng;
 	private Task ref;
+	private long seed;
+	private DoubleVbl.Mean average = new DoubleVbl.Mean();
 
-	// change prng to seed
-	// add task reference to constructor
-	public Simulation(/*Task ref, long seed,*/ Random prng, int v, double p, int n) {
+	public Simulation(Task ref, long seed, int v, double p, int n) {
 		this.v = v;
 		this.p = p;
 		this.n = n;
-		this.prng = prng;
+		this.seed = seed;
 		this.ref = ref;
 	}
 	
 	
 	
 	public SimulationResult simulate() {
-		double average = 0;
-//		DoubleVbl.Sum average = new DoubleVbl.Sum(0);
-		for(int i = 0; i < n; i++) {
-			average += UndirectedGraph.randomGraph(prng, v, p).averageDistance();
-		}
+		this.ref.parallelFor(0, n - 1).exec(new Loop() {
+			Random prng;
+			DoubleVbl.Mean thrAverage;
+			
+			@Override
+			public void start() {
+				prng = new Random(seed + rank());
+				thrAverage = threadLocal(average);
+			}
+			@Override
+			public void run(int i) {
+				thrAverage.accumulate(UndirectedGraph.randomGraph(prng, v, p).averageDistance());
+			}
+			
+		});
 		
-		
-//		average /= n;
-		// 1 have task object
-		// have reference to task
-//		this.ref.parallelFor(0, n - 1).exec(new Loop() {
-//
-//			@Override
-//			public void run(int i) {
-//				
-//			}
-//			
-//		});
-		// 2 parallel loop
-		// 3 per thread random number generator
-		// seed + rank() 
-		// reduction variable
-		// DoubleVbl.Sum 
-		
-		return new SimulationResult(v, p, average);
+		return new SimulationResult(v, p, average.doubleValue());
 	}
 }
