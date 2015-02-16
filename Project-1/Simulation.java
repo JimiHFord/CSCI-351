74df80be83b8315c0af5e6298b0a9ccfd81d8101
Jimi Ford <jford@vistaprint.com>
2015-02-16 10:42:13 -0500

    Beginning parallelization
    going to branch before i try this

--- Project-1/Simulation.java
+++ Project-1/Simulation.java
@@ -1,3 +1,6 @@
+import edu.rit.pj2.Loop;
+import edu.rit.pj2.Task;
+import edu.rit.pj2.vbl.DoubleVbl;
 import edu.rit.util.Random;
 
 
@@ -6,23 +9,45 @@ public class Simulation {
 	private int v, n;
 	private double p;
 	private Random prng;
-	
+	private Task ref;
 
-	public Simulation(Random prng, int v, double p, int n) {
+	// change prng to seed
+	// add task reference to constructor
+	public Simulation(/*Task ref, long seed,*/ Random prng, int v, double p, int n) {
 		this.v = v;
 		this.p = p;
 		this.n = n;
 		this.prng = prng;
+		this.ref = ref;
 	}
 	
 	
 	
 	public SimulationResult simulate() {
 		double average = 0;
+//		DoubleVbl.Sum average = new DoubleVbl.Sum(0);
 		for(int i = 0; i < n; i++) {
 			average += UndirectedGraph.randomGraph(prng, v, p).averageDistance();
 		}
-		average /= n;
+		
+		
+//		average /= n;
+		// 1 have task object
+		// have reference to task
+//		this.ref.parallelFor(0, n - 1).exec(new Loop() {
+//
+//			@Override
+//			public void run(int i) {
+//				
+//			}
+//			
+//		});
+		// 2 parallel loop
+		// 3 per thread random number generator
+		// seed + rank() 
+		// reduction variable
+		// DoubleVbl.Sum 
+		
 		return new SimulationResult(v, p, average);
 	}
 }
