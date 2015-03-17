bd13d9d56636d4b0b0e5202a345b6397adb341a2
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-03-16 21:45:38 -0400

    fixed off by one error

--- Project-2/Cricket.java
+++ Project-2/Cricket.java
@@ -1,7 +1,8 @@
 
 public class Cricket extends Vertex {
 
-	private boolean[] chirp = new boolean[3];
+//	private boolean[] chirp = new boolean[3];
+	private boolean[] chirp = new boolean[2];
 	private boolean willChirp;
 	private int currentTick = 0;
 	private final CricketObserver observer;
@@ -27,15 +28,16 @@ public class Cricket extends Vertex {
 	}
 	
 	private void hearChirp() {
-		chirp[2] = true;
+		chirp[1] = true;
 	}
 	
 	public void timeTick(int tick) {
 		currentTick = tick;
 		willChirp = chirp[0];
 		chirp[0] = chirp[1];
-		chirp[1] = chirp[2];
-		chirp[2] = false;
+//		chirp[1] = chirp[2];
+//		chirp[2] = false;
+		chirp[1] = false;
 	}
 	
 	public boolean directFlight(Cricket other) {
