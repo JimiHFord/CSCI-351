cd77a47dee7780d45a64e65d2416f9bb0886e16d
Jimi Ford <jimifordmail@gmail.com>
2015-05-02 23:31:37 -0400

    maybe done?

---
+++ Project-4/Host.java
@@ -0,0 +1,35 @@
+import edu.rit.sim.Simulation;
+
+/**
+ * Class Host provides the server in the web simulation. The server's
+ * request processing time is exponentially distributed with a given mean.
+ * Requests are added to the server's queue at any time. The queue has a given
+ * maximum size.
+ *
+ * @author  Alan Kaminsky
+ * @author Jimi Ford (jhf3617)
+ * @version 22-Apr-2015
+ */
+public class Host extends Routable
+{
+
+	/**
+	 * Construct a new server. The server's request processing time is
+	 * exponentially distributed with the given mean. The server's request queue
+	 * has the given maximum size.
+	 *
+	 * @param  sim    Simulation.
+	 * @param  tproc  Mean request processing time.
+	 * @param  qmax   Maximum queue size.
+	 * @param  prng   Pseudorandom number generator.
+	 */
+	public Host(Simulation sim) {
+		super(sim);
+	}
+
+	@Override
+	public void receivePacket(Packet packet, Link link) {
+		link.open();
+		packet.finish();
+	}
+}
---
+++ Project-4/Packet.java
@@ -0,0 +1,76 @@
+import edu.rit.numeric.ListSeries;
+import edu.rit.sim.Simulation;
+import edu.rit.util.Random;
+
+/**
+ * Class Request provides a request in the web simulation.
+ *
+ * @author  Alan Kaminsky
+ * @version 18-Apr-2014
+ */
+public class Packet
+{
+	public static final int BIT_RATE = 9600;
+	public final int size;
+	
+	private static int idCounter = 0;
+
+	private int id;
+	private Simulation sim;
+	private double startTime;
+	private double finishTime;
+	private ListSeries respTimeSeries;
+
+
+	/**
+	 * Construct a new request. The request's start time is set to the current
+	 * simulation time. The request's response time will be recorded in the
+	 * given series.
+	 *
+	 * @param  sim     Simulation.
+	 * @param  series  Response time series.
+	 */
+	public Packet(Random prng, Simulation sim, ListSeries series) {
+		this.id = ++ idCounter;
+		this.sim = sim;
+		this.startTime = sim.time();
+		this.size = prng.nextDouble() < .5 ? 
+				40 * Byte.SIZE : 576 * Byte.SIZE;
+		this.respTimeSeries = series;
+	}
+	
+	public double transmitTime() {
+		return ((double)size) / BIT_RATE;
+	}
+	
+	/**
+	 * Mark this request as finished. The request's finish time is set to the
+	 * current simulation time. The request's response time is recorded in the
+	 * response time series.
+	 */
+	public void finish()
+	{
+		finishTime = sim.time();
+		if (respTimeSeries != null) respTimeSeries.add (responseTime());
+	}
+
+	/**
+	 * Returns this request's response time.
+	 *
+	 * @return  Response time.
+	 */
+	public double responseTime()
+	{
+		return finishTime - startTime;
+	}
+
+	/**
+	 * Returns a string version of this request.
+	 *
+	 * @return  String version.
+	 */
+	public String toString()
+	{
+		return "Request " + id;
+	}
+}
